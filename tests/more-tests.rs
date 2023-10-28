9eb421b69b355f349fd693241a995d0678ed9ce0
ntalbs <ntalbsen@gmail.com>
2023-10-28 12:08:28 +0100

    Add more tests: read json files and test them

--- tests/more-tests.rs
+++ tests/more-tests.rs
@@ -1,5 +1,6 @@
-use std::fs::read_to_string;
 use std::error::Error;
+use std::fs::{self, read_to_string};
+use std::io::Write;
 use std::str::FromStr;
 
 use json_parser::Json;
@@ -8,14 +9,22 @@ type TestResult = Result<(), Box<dyn Error>>;
 
 #[test]
 fn parse_json_from_json_files() -> TestResult {
-    let input = read_to_string("tests/inputs/list-lambda.json")?;
-
-    match Json::from_str(input.as_str()) {
-        Ok(json) => println!("{json}"),
-        Err(_) => {
-            panic!("failed to parse input json");
+    let paths = fs::read_dir("tests/inputs")?;
+    for f in paths {
+        let dir_entry = f?;
+        let path = dir_entry.path();
+        let file = path.to_str().unwrap();
+        let input = read_to_string(file)?;
+        println!(">>> {file}");
+        match Json::from_str(input.as_str()) {
+            Ok(json) => println!("{json}"),
+            Err(e) => {
+                println!(">>> Failed to parse {file}");
+                println!("{e:?}");
+                std::io::stdout().flush()?;
+            }
         }
-    };
+    }
 
-   Ok(())
+    Ok(())
 }
