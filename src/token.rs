89d24b5680865379b1142a990d19eace093ab62d
ntalbs <ntalbsen@gmail.com>
2024-12-06 10:01:24 +0000

    Remove unnecessary lifetime annotation

--- src/token.rs
+++ src/token.rs
@@ -66,7 +66,7 @@ impl<'a> Token<'a> {
     }
 }
 
-impl<'a> Display for Token<'a> {
+impl Display for Token<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::LeftBrace { .. }
