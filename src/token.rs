6d867936d5ef9e3e3decd94cdeda354bf1f665e1
ntalbs <ntalbsen@gmail.com>
2026-04-13 09:34:52 +0100

    Add tests for Token

--- src/token.rs
+++ src/token.rs
@@ -91,3 +91,68 @@ impl Display for Token<'_> {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use p_test::p_test;
+
+    use crate::{Pos, token::Token};
+
+    const POS: Pos = Pos { line: 1, col: 1 };
+
+    #[p_test(
+        (Token::LeftBrace(POS)),
+        (Token::RightBrace(POS)),
+        (Token::LeftBracket(POS)),
+        (Token::RightBracket(POS)),
+        (Token::Colon(POS)),
+        (Token::Comma(POS)),
+        (Token::String { lexeme: "dummy", val: "dummy", pos: POS} ),
+        (Token::Number { lexeme: "100", val: 100.0, pos: POS} ),
+        (Token::Bool { lexeme: "true", val: true, pos: POS} ),
+        (Token::Null { lexeme: "null", pos: POS} ),
+        (Token::Eof(POS)),
+    )]
+    fn test_token_pos(token: Token) {
+        assert_eq!(token.pos(), POS);
+    }
+
+    #[p_test(
+        (Token::LeftBrace(POS), "{"),
+        (Token::RightBrace(POS), "}"),
+        (Token::LeftBracket(POS), "["),
+        (Token::RightBracket(POS), "]"),
+        (Token::Colon(POS), ":"),
+        (Token::Comma(POS), ","),
+        (Token::String { lexeme: "dummy", val: "dummy", pos: POS}, "dummy" ),
+        (Token::Number { lexeme: "100", val: 100.0, pos: POS}, "100" ),
+        (Token::Bool { lexeme: "true", val: true, pos: POS}, "true" ),
+        (Token::Null { lexeme: "null", pos: POS}, "null" ),
+    )]
+    fn test_token_lexeme(token: Token, lexeme: &str) {
+        assert_eq!(token.lexeme(), lexeme);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_token_lexeme_eof() {
+        Token::Eof(POS).lexeme();
+    }
+
+    #[p_test(
+        (Token::LeftBrace(POS), "'{' at 1:1"),
+        (Token::RightBrace(POS), "'}' at 1:1"),
+        (Token::LeftBracket(POS), "'[' at 1:1"),
+        (Token::RightBracket(POS), "']' at 1:1"),
+        (Token::Colon(POS), "':' at 1:1"),
+        (Token::Comma(POS), "',' at 1:1"),
+        (Token::String { lexeme: "dummy", val: "dummy", pos: POS}, "'dummy' => \"dummy\" at 1:1"),
+        (Token::Number { lexeme: "100", val: 100.0, pos: POS}, "'100' => 100 at 1:1"),
+        (Token::Bool { lexeme: "true", val: true, pos: POS}, "'true' => true at 1:1"),
+        (Token::Null { lexeme: "null", pos: POS}, "'null' => null at 1:1"),
+        (Token::Eof(POS), "EOF"),
+    )]
+    fn test_token_to_string(token: Token, expected: &str) {
+        assert_eq!(token.to_string(), expected);
+    }
+}
